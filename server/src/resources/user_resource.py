f62537b8605413fc7ff3cd38cf6077e29964b6d2
javierpb275 <javierpb275@gmail.com>
2022-10-08 18:06:23 +0200

    server: added error handling in user_resource

--- server/src/resources/user_resource.py
+++ server/src/resources/user_resource.py
@@ -25,47 +25,61 @@ class UserSignUp(Resource):
 
     @classmethod
     def post(cls):
-        data = _user_parser.parse_args()
-        if not data['email'] or not data['username'] or not data['password']:
-            return {"error": True, "data": {"message": "Email, Username or Password is missing"}}, 400
-        if not data['accept_terms_conditions']:
-            return {"error": True, "data": {"message": "You must accept the terms and conditions."}}, 400
-        if UserModel.find_one(email=data['email']):
-            return {"error": True, "data": {"message": "Email or Username is not valid. Try Again"}}, 400
-        data['password'] = generate_password_hash(data['password'])
-        user = UserModel(**data)
-        user.save_to_db()
-        access_token = create_access_token(identity=user.user_id, fresh=True)
-        refresh_token = create_refresh_token(identity=user.user_id)
-        return {"error": False,
-                "data": {
-                    "message": "User created successfully",
-                    "access_token": access_token,
-                    "refresh_token": refresh_token,
-                    "user": user.json()
-                }}, 201
+        try:
+            data = _user_parser.parse_args()
+            if not data['email'] or not data['username'] or not data['password']:
+                return {"error": True, "data": {"message": "Email, Username or Password is missing"}}, 400
+            if not data['accept_terms_conditions']:
+                return {"error": True, "data": {"message": "You must accept the terms and conditions."}}, 400
+            if UserModel.find_one(email=data['email']):
+                return {"error": True, "data": {"message": "Email or Username is not valid. Try Again"}}, 400
+            data['password'] = generate_password_hash(data['password'])
+            user = UserModel(**data)
+            user.save_to_db()
+            access_token = create_access_token(identity=user.user_id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.user_id)
+            return {"error": False,
+                    "data": {
+                        "message": "User created successfully",
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                        "user": user.json()
+                    }}, 201
+        except Exception as e:
+            return {"error": True,
+                    "data": {
+                        "message": "Error Signing Up",
+                        "error": str(e)
+                    }}, 400
 
 
 class UserSignIn(Resource):
 
     @classmethod
     def post(cls):
-        data = _user_parser.parse_args()
-        if not data['email'] or not data['password']:
-            return {"error": True, "data": {"message": "Email or Password is missing"}}, 400
-        user = UserModel.find_one(email=data['email'])
-        if user and check_password_hash(user.password, data['password']):
-            access_token = create_access_token(
-                identity=user.user_id, fresh=True)
-            refresh_token = create_refresh_token(identity=user.user_id)
-            return {"error": False,
+        try:
+            data = _user_parser.parse_args()
+            if not data['email'] or not data['password']:
+                return {"error": True, "data": {"message": "Email or Password is missing"}}, 400
+            user = UserModel.find_one(email=data['email'])
+            if user and check_password_hash(user.password, data['password']):
+                access_token = create_access_token(
+                    identity=user.user_id, fresh=True)
+                refresh_token = create_refresh_token(identity=user.user_id)
+                return {"error": False,
+                        "data": {
+                            "message": "Logged In Successfully",
+                            "access_token": access_token,
+                            "refresh_token": refresh_token,
+                            "user": user.json()
+                        }}, 200
+            return {"error": True, "data": {"message": "Invalid Credentials"}}, 401
+        except Exception as e:
+            return {"error": True,
                     "data": {
-                        "message": "Logged In Successfully",
-                        "access_token": access_token,
-                        "refresh_token": refresh_token,
-                        "user": user.json()
-                    }}, 200
-        return {"error": True, "data": {"message": "Invalid Credentials"}}, 401
+                        "message": "Error Signing In",
+                        "error": str(e)
+                    }}, 400
 
 
 class UserSignOut(Resource):
@@ -74,31 +88,45 @@ class UserSignOut(Resource):
 
     @jwt_required()
     def post(self):
-        data = self.parser.parse_args()
-        if not data['refresh_token']:
-            return {"error": True, "data": {"message": "No Refresh Token was Provided"}}, 400
-        decoded_refresh_token = jwt.decode(data['refresh_token'], options={
-                                           "verify_signature": False})
-        jti_access_token = get_jwt()['jti']
-        jti_refresh_token = decoded_refresh_token['jti']
-        BLOCKLIST.add(jti_access_token)
-        BLOCKLIST.add(jti_refresh_token)
-        return {"error": False, "data": {"message": "Logged Out Successfully"}}, 200
+        try:
+            data = self.parser.parse_args()
+            if not data['refresh_token']:
+                return {"error": True, "data": {"message": "No Refresh Token was Provided"}}, 400
+            decoded_refresh_token = jwt.decode(data['refresh_token'], options={
+                                            "verify_signature": False})
+            jti_access_token = get_jwt()['jti']
+            jti_refresh_token = decoded_refresh_token['jti']
+            BLOCKLIST.add(jti_access_token)
+            BLOCKLIST.add(jti_refresh_token)
+            return {"error": False, "data": {"message": "Logged Out Successfully"}}, 200
+        except Exception as e:
+            return {"error": True,
+                    "data": {
+                        "message": "Error Signing Out",
+                        "error": str(e)
+                    }}, 500
 
 
 class TokenRefresh(Resource):
 
     @jwt_required(refresh=True)
     def post(self):
-        current_user = get_jwt_identity()
-        access_token = create_access_token(identity=current_user, fresh=False)
-        refresh_token = create_refresh_token(identity=current_user)
-        return {"error": False,
-                "data": {
-                    "message": "Refreshed Token Successfully",
-                    "access_token": access_token,
-                    "refresh_token": refresh_token,
-                }}, 200
+        try:
+            current_user = get_jwt_identity()
+            access_token = create_access_token(identity=current_user, fresh=False)
+            refresh_token = create_refresh_token(identity=current_user)
+            return {"error": False,
+                    "data": {
+                        "message": "Refreshed Token Successfully",
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    }}, 200
+        except Exception as e:
+            return {"error": True,
+                    "data": {
+                        "message": "Error Refreshing Token",
+                        "error": str(e)
+                    }}, 500
 
 
 class Profile(Resource):
@@ -107,30 +135,44 @@ class Profile(Resource):
 
     @jwt_required()
     def get(self):
-        user_id = get_jwt_identity()
-        user = UserModel.find_one(user_id=user_id)
-        if not user:
-            return {"error": True, "data": {"message": "User Not Found"}}, 404
-        return {"error": False,
-                "data": {
-                    "message": "Profile obtained successfully.",
-                    "user": user.json()
-                }}, 200
+        try:
+            user_id = get_jwt_identity()
+            user = UserModel.find_one(user_id=user_id)
+            if not user:
+                return {"error": True, "data": {"message": "User Not Found"}}, 404
+            return {"error": False,
+                    "data": {
+                        "message": "Profile obtained successfully.",
+                        "user": user.json()
+                    }}, 200
+        except Exception as e:
+            return {"error": True,
+                    "data": {
+                        "message": "Error Getting Profile",
+                        "error": str(e)
+                    }}, 500
 
     @jwt_required(fresh=True)
     def delete(self):
-        user_id = get_jwt_identity()
-        user = UserModel.find_one(user_id=user_id)
-        if not user:
-            return {"error": True, "data": {"message": "User Not Found"}}, 404
-        data = self.parser.parse_args()
-        if not data['refresh_token']:
-            return {"error": True, "data": {"message": "No Refresh Token was Provided"}}, 400
-        user.delete_from_db()
-        decoded_refresh_token = jwt.decode(data['refresh_token'], options={
-                                           "verify_signature": False})
-        jti_access_token = get_jwt()['jti']
-        jti_refresh_token = decoded_refresh_token['jti']
-        BLOCKLIST.add(jti_access_token)
-        BLOCKLIST.add(jti_refresh_token)
-        return {"error": False, "data": {"message": "Profile deleted successfully"}}, 200
+        try:
+            user_id = get_jwt_identity()
+            user = UserModel.find_one(user_id=user_id)
+            if not user:
+                return {"error": True, "data": {"message": "User Not Found"}}, 404
+            data = self.parser.parse_args()
+            if not data['refresh_token']:
+                return {"error": True, "data": {"message": "No Refresh Token was Provided"}}, 400
+            user.delete_from_db()
+            decoded_refresh_token = jwt.decode(data['refresh_token'], options={
+                                            "verify_signature": False})
+            jti_access_token = get_jwt()['jti']
+            jti_refresh_token = decoded_refresh_token['jti']
+            BLOCKLIST.add(jti_access_token)
+            BLOCKLIST.add(jti_refresh_token)
+            return {"error": False, "data": {"message": "Profile deleted successfully"}}, 200
+        except Exception as e:
+            return {"error": True,
+                    "data": {
+                        "message": "Error Deleting Profile",
+                        "error": str(e)
+                    }}, 500
